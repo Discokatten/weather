c31cc3c677741726f5b01298de9496a6c7301955
Discokatten <isabelle.wincrantz@gmail.com>
2025-10-09 09:16:08 +0200

    outline for design

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,5 +1,6 @@
 export default function Hero() {
     // background for hero: weatherwidget
-  return <div className="bg-[url(/hero.svg)] bg-no-repeat h-75 w-200">
+  return <div className="bg-[url(/hero.svg)] bg-no-repeat bg-cover bg-center rounded-2xl h-77 max-w-200">
+    {/* TODO - weatherwidget */}
   </div>;
 }
