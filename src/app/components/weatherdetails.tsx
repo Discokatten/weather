d7ea432e80494b422ea184e612182d8761b4fb1e
Discokatten <isabelle.wincrantz@gmail.com>
2025-10-10 10:57:22 +0200

    updated layout and decimals

--- src/app/components/weatherdetails.tsx
+++ src/app/components/weatherdetails.tsx
@@ -6,24 +6,27 @@ export default async function WeatherDetails() {
 
   return (
     <ul className="h-72 flex justify-between content-center text-white">
-      
       <li className="bg-theme-800 rounded-2xl h-35 w-25 content-center text-center">
-        Feels Like {current.apparent_temperature.slice(0, 4)}
+        <p className="mb-2">Feels Like</p>
+        <p>{current.apparent_temperature.slice(0, 3)} °C</p>
         {/* Feels Like 17 °C */}
       </li>
 
-      <li className="bg-theme-800 rounded-2xl h-35 w-25 content-center text-center">
-        Luftfuktighet {current.relative_humidity_2m.slice(0, 4)} %
+      <li className="bg-theme-800 rounded-2xl h-35 w-25 content-center text-center text-pretty">
+        <p className="mb-2">Luftfuktighet</p>
+        <p>{current.relative_humidity_2m.slice(0, 2)}%</p>
         {/* Luftfuktighet 78% */}
       </li>
 
       <li className="bg-theme-800 rounded-2xl h-35 w-25 content-center text-center">
-        Vind {current.wind_speed_10m.slice(0, 2)} m/s
+        <p className="mb-2">Vind</p>
+        <p>{current.wind_speed_10m.slice(0, 3)} m/s</p>
         {/* Vind 13 m/s */}
       </li>
 
       <li className="bg-theme-800 rounded-2xl h-35 w-25 content-center text-center">
-        Förväntad nederbörd {current.precipitation.slice(0, 3)} mm
+        <p className="mb-2">Förväntad nederbörd</p>
+        <p>{current.precipitation.slice(0, 3)} mm</p>
         {/* Nederbörd 3 mm */}
       </li>
     </ul>
