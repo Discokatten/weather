c63fcee06f103416e3a05932a19c15b4da8d0dc8
Discokatten <isabelle.wincrantz@gmail.com>
2025-10-15 10:56:08 +0200

    added daily details

--- src/app/components/timeofday.tsx
+++ src/app/components/timeofday.tsx
@@ -1,29 +1,32 @@
 import getWeather from "../data/api";
+import { TIMEOFDAY } from "../lib/consts";
+import WeatherCard from "./weathercard";
 
 export default async function TimeOfDay() {
   const weather = await getWeather();
   const hourly = weather.hourly;
   const tempArray = Array.from(hourly.temperature_2m!);
-  const timeArray = hourly.time.map((time) => time.getUTCHours());
-
-  const morning = 8;
-  const midday = 12;
-  const evening = 18;
+  const codeArray = Array.from(hourly.weather_code!);
+  const feelsArray = Array.from(hourly.apparent_temperature!);
 
   return (
-    <ul className="flex  justify-around content-center">
-      <li className="bg-theme-800 rounded-2xl h-35 w-25 content-center text-center">
-        <p>time:{timeArray[morning]}</p>
-        <p>temp:{tempArray[morning].toFixed(1)}</p>
-      </li>
-      <li className="bg-theme-800 rounded-2xl h-35 w-25 content-center text-center">
-        <p>time:{timeArray[midday]}</p>
-        <p>temp:{tempArray[midday].toFixed(1)}</p>
-      </li>
-      <li className="bg-theme-800 rounded-2xl h-35 w-25 content-center text-center">
-        <p>time:{timeArray[evening]}</p>
-        <p>temp:{tempArray[evening].toFixed(1)}</p>
-      </li>
+    <ul className="h-72 flex justify-between content-center text-white mt-2">
+      {TIMEOFDAY.map((item, index) => {
+        return (
+          <div
+            key={index}
+            className="bg-theme-800 rounded-2xl h-35 w-25 content-center text-center"
+          >
+            <p>{item.title}</p>
+            <WeatherCard
+              key={index}
+              temp={tempArray[item.time].toFixed(1)}
+              code={codeArray[index]}
+              feels={feelsArray[item.time].toFixed(1)}
+            />
+          </div>
+        );
+      })}
     </ul>
   );
 }
--- src/app/components/weatherdetails.tsx
+++ src/app/components/weatherdetails.tsx
@@ -5,29 +5,25 @@ export default async function WeatherDetails() {
   const current = weather.current;
 
   return (
-    <ul className="flex justify-between content-center text-white">
+    <ul className="flex justify-between content-center text-white mt-2">
       <li className="bg-theme-800 rounded-2xl h-35 w-25 content-center text-center">
-        <p className="mb-2">Feels Like</p>
+        <p className="mb-2">Feels like</p>
         <p>{current.apparent_temperature.slice(0, 3)} °C</p>
-        {/* Feels Like 17 °C */}
       </li>
 
       <li className="bg-theme-800 rounded-2xl h-35 w-25 content-center text-center text-pretty">
         <p className="mb-2">Luftfuktighet</p>
         <p>{current.relative_humidity_2m.slice(0, 2)}%</p>
-        {/* Luftfuktighet 78% */}
       </li>
 
       <li className="bg-theme-800 rounded-2xl h-35 w-25 content-center text-center">
         <p className="mb-2">Vind</p>
         <p>{current.wind_speed_10m.slice(0, 3)} m/s</p>
-        {/* Vind 13 m/s */}
       </li>
 
       <li className="bg-theme-800 rounded-2xl h-35 w-25 content-center text-center">
         <p className="mb-2">Förväntad nederbörd</p>
         <p>{current.precipitation.slice(0, 3)} mm</p>
-        {/* Nederbörd 3 mm */}
       </li>
     </ul>
   );
